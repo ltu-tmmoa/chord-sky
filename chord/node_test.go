236f3b2fe56ac9f327d6b23ec245cb08873fefe7
Emanuel Palm <emanuelpalm@gmail.com>
2016-09-22 10:01:51 +0200

    Update chord/node_test.go

--- chord/node_test.go
+++ chord/node_test.go
@@ -14,12 +14,25 @@ func TestNodeJoin(t *testing.T) {
 
 	nodes[0].Join(nil)
 	nodes[1].Join(nodes[0])
-	/*nodes[2].Join(nodes[1])
+	nodes[2].Join(nodes[1])
 	nodes[3].Join(nodes[1])
 	nodes[4].Join(nodes[1])
 	nodes[5].Join(nodes[4])
 	nodes[6].Join(nodes[3])
-	nodes[7].Join(nodes[3])*/
+	nodes[7].Join(nodes[3])
 
-	nodes[0].PrintRing()
+	for _, node := range nodes {
+		fmt.Printf("Node: %v\n", node)
+		for i := 1; i <= M3; i++ {
+			finger := node.finger(i)
+			start, stop := finger.Interval()
+			fmt.Printf("  Finger:      %v\n", i)
+			fmt.Printf("    Start:       %v\n", finger.start)
+			fmt.Printf("    Interval:    [%v, %v)\n", start.BigInt().Int64(), stop.BigInt().Int64())
+			fmt.Printf("    Node:        %v\n", finger.Node())
+		}
+		fmt.Printf("  Predecessor: %v\n", node.predecessor)
+	}
+
+	//nodes[0].PrintRing()
 }
