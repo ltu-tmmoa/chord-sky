8846bb3419222c21091062f89d529dd2c0dc2ce4
Emanuel Palm <emanuelpalm@gmail.com>
2016-10-19 11:01:30 +0200

    Make Chord PublicNode use mutex

--- chord/public_node.go
+++ chord/public_node.go
@@ -2,11 +2,21 @@ package chord
 
 import (
 	"net"
+	"sync"
 )
 
 // PublicNode is used to expose Chord Node operations via RPC.
 type PublicNode struct {
-	node Node
+	node  Node
+	mutex *sync.RWMutex
+}
+
+// NewPublicNode creates a new public node, wrapping given regular node, synchronizing it using provided mutex.
+func NewPublicNode(node Node, mutex *sync.RWMutex) *PublicNode {
+	publicNode := new(PublicNode)
+	publicNode.node = node
+	publicNode.mutex = mutex
+	return publicNode
 }
 
 // GetFingerNode fetches Chord node at given finger table offset i.
@@ -14,6 +24,9 @@ type PublicNode struct {
 // The result is only defined for i in [1,M], where M is the amount of bits set
 // at node ring creation.
 func (node *PublicNode) GetFingerNode(i *int, reply *net.IPAddr) error {
+	node.mutex.RLock()
+	defer node.mutex.RUnlock()
+
 	fingerNode, err := node.node.Finger(*i).Node()
 	if err != nil {
 		return err
@@ -24,6 +37,9 @@ func (node *PublicNode) GetFingerNode(i *int, reply *net.IPAddr) error {
 
 // GetSuccessor yields public node successor.
 func (node *PublicNode) GetSuccessor(void *int, reply *net.IPAddr) error {
+	node.mutex.RLock()
+	defer node.mutex.RUnlock()
+
 	successor, err := node.node.Successor()
 	if err != nil {
 		return err
@@ -34,6 +50,9 @@ func (node *PublicNode) GetSuccessor(void *int, reply *net.IPAddr) error {
 
 // GetPredecessor yields public node predecessor.
 func (node *PublicNode) GetPredecessor(void *int, reply *net.IPAddr) error {
+	node.mutex.RLock()
+	defer node.mutex.RUnlock()
+
 	predecessor, err := node.node.Predecessor()
 	if err != nil {
 		return err
@@ -44,6 +63,9 @@ func (node *PublicNode) GetPredecessor(void *int, reply *net.IPAddr) error {
 
 // FindSuccessor asks this node to find successor of given ID.
 func (node *PublicNode) FindSuccessor(id *Hash, reply *net.IPAddr) error {
+	node.mutex.RLock()
+	defer node.mutex.RUnlock()
+
 	successor, err := node.node.FindSuccessor(id)
 	if err != nil {
 		return err
@@ -54,6 +76,9 @@ func (node *PublicNode) FindSuccessor(id *Hash, reply *net.IPAddr) error {
 
 // FindPredecessor asks this node to find a predecessor of given ID.
 func (node *PublicNode) FindPredecessor(id *Hash, reply *net.IPAddr) error {
+	node.mutex.RLock()
+	defer node.mutex.RUnlock()
+
 	predecessor, err := node.node.FindPredecessor(id)
 	if err != nil {
 		return err
@@ -64,10 +89,16 @@ func (node *PublicNode) FindPredecessor(id *Hash, reply *net.IPAddr) error {
 
 // SetSuccessor attempts to set this node's successor to given node.
 func (node *PublicNode) SetSuccessor(successor *net.IPAddr, void *int) error {
+	node.mutex.Lock()
+	defer node.mutex.Unlock()
+
 	return node.node.SetSuccessor(NewRemoteNode(successor))
 }
 
 // SetPredecessor attempts to set this node's predecessor to given node.
 func (node *PublicNode) SetPredecessor(predecessor *net.IPAddr, void *int) error {
+	node.mutex.Lock()
+	defer node.mutex.Unlock()
+
 	return node.node.SetPredecessor(NewRemoteNode(predecessor))
 }
