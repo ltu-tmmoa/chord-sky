98a501a589399ec0e1d23fd812f0e27576bbe7d8
Emanuel Palm <emanuelpalm@gmail.com>
2016-10-19 10:35:42 +0200

    Fix net GetLocalIPAddr()

--- net/net.go
+++ net/net.go
@@ -11,12 +11,27 @@ func GetLocalIPAddr() (*net.IPAddr, error) {
 	if err != nil {
 		return nil, err
 	}
+	ipNet := findIPNetIn(addrs, func(ipNet *net.IPNet) bool {
+		return ipNet.IP.To4() != nil
+	})
+	if ipNet == nil {
+		ipNet = findIPNetIn(addrs, func(ipNet *net.IPNet) bool {
+			return ipNet.IP.To16() != nil
+		})
+	}
+	if ipNet != nil {
+		return net.ResolveIPAddr("ip", ipNet.IP.String())
+	}
+	return nil, errors.New("No suitable IP interface available.")
+}
+
+func findIPNetIn(addrs []net.Addr, predicate func(*net.IPNet) bool) *net.IPNet {
 	for _, addr := range addrs {
-		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				return addr.(*net.IPAddr), nil
+		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if predicate(ipNet) {
+				return ipNet
 			}
 		}
 	}
-	return nil, errors.New("No suitable IP interface available.")
+	return nil
 }
