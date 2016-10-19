7f1e275049ca4ff5c9376c4b5088c1cb18f08f23
Emanuel Palm <emanuelpalm@gmail.com>
2016-10-19 11:28:45 +0200

    Make main.go handle Chord node joins

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"net/http"
 	"net/rpc"
+	"strings"
 	"sync"
 	"time"
 
@@ -13,7 +15,15 @@ import (
 	cnet "github.com/ltu-tmmoa/chord-sky/net"
 )
 
+var peer string
+
+func init() {
+	flag.StringVar(&peer, "peer", "", "<IP:PORT> of Chord Sky Node to join. If not given a new ring is created.")
+}
+
 func main() {
+	flag.Parse()
+
 	log.Logger.Println("Chord Sky")
 
 	// Setup chord node representing this machine.
@@ -24,7 +34,25 @@ func main() {
 			log.Logger.Fatalln(err)
 		}
 		localNode = chord.NewLocalNode(localIPAddr)
-		log.Logger.Println("Local address:", localNode.IPAddr())
+	}
+
+	// Join new or existing Chord ring.
+	{
+		trimmedPeer := strings.TrimSpace(peer)
+		if len(trimmedPeer) == 0 {
+			log.Logger.Println("No peer specified. Forming new ring ...")
+
+			localNode.Join(nil)
+
+		} else {
+			log.Logger.Println("Joining ring via", trimmedPeer, "...")
+
+			ipAddr, err := net.ResolveIPAddr("ip", trimmedPeer)
+			if err != nil {
+				log.Logger.Fatalln(err)
+			}
+			localNode.Join(chord.NewRemoteNode(ipAddr))
+		}
 	}
 
 	localNodeMutex := &sync.RWMutex{}
@@ -33,6 +61,8 @@ func main() {
 	go func() {
 		for {
 			time.Sleep(30 * time.Second)
+
+			// TODO: Heartbeat?
 
 			localNodeMutex.Lock()
 			defer localNodeMutex.Unlock()
@@ -57,7 +87,7 @@ func main() {
 		if err != nil {
 			log.Logger.Fatalln(err)
 		}
-		log.Logger.Println("Listening on:", tcpAddr)
+		log.Logger.Println("Listening on", tcpAddr, "...")
 
 		http.Serve(listner, nil)
 	}
