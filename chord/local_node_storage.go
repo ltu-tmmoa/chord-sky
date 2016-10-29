e0a10d7aefb256ef95d1390e7acc24e4452617c0
Emanuel Palm <emanuelpalm@gmail.com>
2016-10-29 15:49:27 +0200

    Implement localNode#{uploadStorageTo,downloadStorageFrom}()

--- chord/local_node_storage.go
+++ chord/local_node_storage.go
@@ -1,16 +1,36 @@
 package chord
 
+import (
+	"github.com/ltu-tmmoa/chord-sky/log"
+)
+
 func (node *localNode) downloadStorageOf(peer Node) error {
+	log.Logger.Println("Downloading storage from", peer, "...")
 	return transferStorage(peer, node)
 }
 
 func (node *localNode) uploadStorageTo(peer Node) error {
+	log.Logger.Println("Uploading storage to", peer, "...")
 	return transferStorage(node, peer)
 }
 
 func transferStorage(fromNode, toNode Node) error {
 	if fromNode.ID().Eq(toNode.ID()) {
 		return nil
+	}
+	fromStorage := fromNode.Storage()
+	toStorage := toNode.Storage()
+
+	keys, err := fromStorage.GetKeyRange(toNode.ID(), fromNode.ID())
+	if err != nil {
+		return err
+	}
+	for _, key := range keys {
+		value, err := fromStorage.Get(key)
+		if err != nil {
+			return err
+		}
+		toStorage.Set(key, value)
 	}
 	return nil
 }
