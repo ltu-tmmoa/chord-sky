aff33b3a4568f5ef8b5d2b4df72d5c023692b970
Haidar Chikh <haidarchikh@gmail.com>
2016-11-01 17:43:51 +0100

    Register the storage service

--- main.go
+++ main.go
@@ -61,8 +61,11 @@ func main() {
 		}
 	}()
 
+	  storageService := chord.NewHTTPStorageService(laddr)
+
 	log.Logger.Println("Accepting incoming connections on", laddr, "...")
 	http.Handle("/node/", http.StripPrefix("/node", chordService))
+	  http.Handle("/storage/", http.StripPrefix("/storage", storageService))
 	httpServer := http.Server{
 		Addr:         laddr.String(),
 		ReadTimeout:  5 * time.Second,
